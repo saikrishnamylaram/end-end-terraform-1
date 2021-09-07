90a8d040d02bf3c8bc73e1bc2ec8dac80fe3df6e
saikrishnamylaram <75681251+saikrishnamylaram@users.noreply.github.com>
2021-09-07 15:42:10 +0530

    Update create_aws_ecr_repo.sh

--- create_aws_ecr_repo.sh
+++ create_aws_ecr_repo.sh
@@ -1,9 +1,9 @@
 # RUN me where kubectl is available,& make sure to replace account,region etc
 #
-ACCOUNT=747949668188
-REGION=ap-south-1
+ACCOUNT=831103659601
+REGION=us-east-2
 SECRET_NAME=${REGION}-ecr-registry
-EMAIL=saikrishna01234567@gmail.com
+EMAIL=sneharao983@gmail.com
 
 TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
 
